08ccd974d74223188c423ecfae34473ec21f77ee
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-05-20 15:28:01 +0530

    Variables list Updated!

--- vars.py
+++ vars.py
@@ -6,4 +6,7 @@ if os.path.exists('config.env'):
   load_dotenv('config.env')
   
 class Var(object):
-      referrer = str(getenv('warp_id'))
+      WARP_ID = str(getenv('WARP_ID'))
+      SEND_LOG = str(getenv('SEND_LOG'))
+      BOT_TOKEN = str(getenv('BOT_TOKEN'))
+      CHANNEL_ID = str(getenv('CHANNEL_ID'))
