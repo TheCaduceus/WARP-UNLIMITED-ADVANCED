c9a6ee59be31ecb044f9f5e7adbcd3d6ad140570
Dr.Caduceus <87380104+TheCaduceus@users.noreply.github.com>
2022-05-21 22:35:28 +0530

    HIDE ID Option Added!

--- warp-plus.py
+++ warp-plus.py
@@ -10,6 +10,7 @@ from vars import Var
 SEND_LOG = (Var.SEND_LOG)
 BOT_TOKEN = (Var.BOT_TOKEN)
 CHANNEL_ID = (Var.CHANNEL_ID)
+HIDE_ID = (Var.HIDE_ID)
 referrer = (Var.WARP_ID)
 
 os.system("title UnlimitedWrapUsage")
@@ -67,8 +68,11 @@ while True:
 	result = run()
 	if result == 200:
 		g += 1
-		if SEND_LOG == "1":
-                    urllib.request.urlopen("https://api.telegram.org/bot"+ BOT_TOKEN + "/sendMessage?chat_id=" + CHANNEL_ID + "&text=" + "WARP%20ID:%20" + referrer + "%20DATA%20RECEIVED:%20" + str(g) + "GB%20FAILED%20ATTEMPTs:%20" + str(b))
+		if(SEND_LOG == "1"):
+			if(HIDE_ID == "1"):
+				urllib.request.urlopen("https://api.telegram.org/bot"+ BOT_TOKEN + "/sendMessage?chat_id=" + CHANNEL_ID + "&text=" + "DATA%20RECEIVED:%20" + str(g) + "GB%20FAILED%20ATTEMPT:%20" + str(b))
+			else:
+				urllib.request.urlopen("https://api.telegram.org/bot"+ BOT_TOKEN + "/sendMessage?chat_id=" + CHANNEL_ID + "&text=" + "WARP%20ID:%20" + referrer + "%20DATA%20RECEIVED:%20" + str(g) + "GB%20FAILED%20ATTEMPT:%20" + str(b))
 		print(f"\n[•] WARP+ ID: {referrer}")
 		print(f"[✓] Added: {g} GB")
 		print(f"[#] Total: {g} Good {b} Bad")
